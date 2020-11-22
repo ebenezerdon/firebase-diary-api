f7f7f5a7ceb82c12b07219d851debe65fb4a8018
Ebenezer Don <ebenezerdonu@gmail.com>
2020-11-22 01:27:30 +0100

    replace `\\n` with `\n` in private key

--- functions/src/config/firebase.ts
+++ functions/src/config/firebase.ts
@@ -3,7 +3,7 @@ import * as functions from 'firebase-functions';
 
 admin.initializeApp({
   credential: admin.credential.cert({
-    privateKey: functions.config().private.key,
+    privateKey: functions.config().private.key.replace(/\\n/g, '\n'),
     projectId: functions.config().project.id,
     clientEmail: functions.config().client.email
   }),
