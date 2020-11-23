ccacc18cc9b1b86b865d2fba7e5b823dd0962fb6
Ebenezer Don <ebenezerdonu@gmail.com>
2020-11-23 21:16:15 +0100

    add deleteEntry controller

--- functions/src/entryController.ts
+++ functions/src/entryController.ts
@@ -18,19 +18,18 @@ const addEntry = async (req: Request, res: Response) => {
 
   try {
     const entry = db.collection('entries').doc()
-    entry.set({
-      title, text, userId, coverImageUrl: coverImageUrl || ''
-    })
+    const entryObject = {
+      id: entry.id,
+      title,
+      text,
+      userId,
+      coverImageUrl: coverImageUrl || ''
+    }
+    entry.set(entryObject)
     res.status(200).send({
       status: 'success',
       message: 'entry added successfully',
-      data: {
-        id: entry.id,
-        title,
-        text,
-        userId,
-        coverImageUrl,
-      }
+      data: entryObject
     })
   } catch(error) { res.status(500).json(error.message) }
 }
@@ -51,17 +50,7 @@ const updateEntry = async (req: Request, res: Response) => {
 
   try {
     const entry = db.collection('entries').doc(entryId)
-    const currentData = (await entry.get()).data()
-
-    if (!currentData) return res.status(404).json({
-      status: 'error',
-      message: 'entry with provided id not found'
-    })
-
-    if (currentData.userId !== userId) return res.status(401).json({
-      status: 'error',
-      message: 'user unauthorized'
-    })
+    const currentData = (await entry.get()).data() || {}
 
     const entryObject = {
       imageUrl: coverImageUrl || currentData.coverImageUrl,
@@ -81,7 +70,29 @@ const updateEntry = async (req: Request, res: Response) => {
       message: 'entry updated successfully',
       data: entryObject
     })
-  } catch(error) { return res.status(500).json(error.message) }
+  }
+  catch(error) { return res.status(500).json(error.message) }
 }
 
-export { addEntry, getAllEntries, updateEntry }
+const deleteEntry = async (req: Request, res: Response) => {
+  const { entryId } = req.params
+
+  try {
+    const entry = db.collection('entries').doc(entryId)
+
+    await entry.delete().catch(error => {
+      return res.status(400).json({
+        status: 'error',
+        message: error.message
+      })
+    })
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'entry deleted successfully',
+    })
+  }
+  catch(error) { return res.status(500).json(error.message) }
+}
+
+export { addEntry, getAllEntries, updateEntry, deleteEntry }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,15 @@
 import * as functions from 'firebase-functions'
 import * as express from 'express'
 import { isUserAuthorized } from './auth'
-import { addEntry, getAllEntries, updateEntry } from './entryController'
+import { addEntry, getAllEntries, updateEntry, deleteEntry } from './entryController'
+import { doesEntryExist } from './entryMiddleware'
 
 const app = express()
 
 app.get('/', (req, res) => res.status(200).send('Hey there!'))
 app.post('/entry', isUserAuthorized, addEntry)
 app.get('/entries', isUserAuthorized, getAllEntries)
-app.patch('/entried/:entryId', isUserAuthorized, updateEntry)
+app.patch('/entries/:entryId', doesEntryExist, isUserAuthorized, updateEntry)
+app.delete('/entries/:entryId', doesEntryExist, isUserAuthorized, deleteEntry)
 
 exports.app = functions.https.onRequest(app)
