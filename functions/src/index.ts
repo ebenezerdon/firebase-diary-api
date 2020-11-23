8d6dd1521101c6d3f8becff61d2d28fe8c90455d
Ebenezer Don <ebenezerdonu@gmail.com>
2020-11-23 18:47:57 +0100

    add updateEntry route

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,13 @@
 import * as functions from 'firebase-functions'
 import * as express from 'express'
 import { isUserAuthorized } from './auth'
-import { addEntry, getAllEntries } from './entryController'
+import { addEntry, getAllEntries, updateEntry } from './entryController'
 
 const app = express()
 
 app.get('/', (req, res) => res.status(200).send('Hey there!'))
 app.post('/entry', isUserAuthorized, addEntry)
 app.get('/entries', isUserAuthorized, getAllEntries)
+app.patch('/entried/:entryId', isUserAuthorized, updateEntry)
 
 exports.app = functions.https.onRequest(app)
